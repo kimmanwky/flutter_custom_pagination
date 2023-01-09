84b6ea1dfc94a95891a077b7f1cacc069d2d3f13
Kimman <kimman.wky@gmail.com>
2023-01-09 15:23:30 +0800

    init

---
+++ example/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_custom_pagination/flutter_custom_pagination.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Custom Pagination Sample',
+      home: MyHomePage(title: 'Flutter Custom Pagination Sample'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title!),
+      ),
+      body: const Center(
+        child: FlutterCustomPagination(),
+      ),
+    );
+  }
+}
---
+++ lib/flutter_custom_pagination.dart
@@ -0,0 +1,18 @@
+library flutter_custom_pagination;
+
+import 'package:flutter/material.dart';
+
+class FlutterCustomPagination extends StatefulWidget {
+  const FlutterCustomPagination({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _FlutterCustomPaginationState createState() => _FlutterCustomPaginationState();
+}
+
+class _FlutterCustomPaginationState extends State<FlutterCustomPagination> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('This is flutter custom pagination'));
+  }
+}
